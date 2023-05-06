6d593b7edb867a24c213439905983509bc77bab5
mievenfall <mngoc2603@gmail.com>
2023-05-06 01:14:38 -0700

    Update Rooms and the Maze

--- main.py
+++ main.py
@@ -12,19 +12,247 @@ from colors import *
 from wallsRooms import *
 from player import *
 
-def createRoomList():
-    roomsList = [RoomFull(),
-                 RoomFull(),
-                 RoomFullTop(),
-                 RoomFull(),
-                 RoomFullLeft(),
-                 RoomFullTop(),
-                 RoomFullBottom(),
-                 RoomFullTop(),
-                 RoomFull(),
-                 RoomFull(),
-                 RoomFullBottom()]
+def createRoomsList():
+    roomsList = [RoomStart(0),
+                 RoomFull(1),
+                 RoomFull(2),
+                 RoomFullTop(3),
+                 RoomFull(4),
+                 RoomFullLeft(5),
+                 RoomFullTop(6),
+                 RoomFullTopBottom(7),
+                 RoomFullTop(8),
+                 RoomFullLeftRight(9),
+                 RoomFull(10),
+                 RoomFullBottom(11)]
     return roomsList
+
+def topPos(player):
+    return 300, 0
+
+def botPos(player):
+    return 320, 580
+
+def leftPos(player):
+    return 0, 300
+
+def rightPos(player):
+    return  790, 300
+
+def goLeft(num, roomsList, player):
+    # ROOM 1 -> ROOM 2
+    if num == 1: 
+        num = 2
+        player.rect.x, player.rect.y = topPos(player)
+
+    # ROOM 2 -> ROOM 2
+    if num == 2:
+        num = 2
+        player.rect.x, player.rect.y = leftPos(player)
+
+    # ROOM 3 -> ROOM 1
+    if num == 3:
+        num = 1
+        player.rect.x, player.rect.y = rightPos(player)
+
+    # ROOM 4 -> ROOM 1
+    if num == 4:
+        num = 1   
+        player.rect.x, player.rect.y = botPos(player)
+
+    # ROOM 5 XX
+
+    # ROOM 6 -> ROOM 7
+    if num == 6:
+        num = 7   
+        player.rect.x, player.rect.y = rightPos(player)
+
+    # ROOM 7 -> ROOM 8
+    if num == 7:
+        num = 8   
+        player.rect.x, player.rect.y = leftPos(player)
+
+    # ROOM 8 -> ROOM 7
+    if num == 8:
+        num = 7   
+        player.rect.x, player.rect.y = leftPos(player)
+
+    # ROOM 9 xxx
+
+    # ROOM 10 -> ROOM 6
+    if num == 10:
+        num = 6
+        player.rect.x, player.rect.y = botPos(player)
+
+    # ROOM 11 -> ROOM 10
+    if num == 11:
+        num = 10
+        player.rect.x, player.rect.y = topPos(player)
+
+
+    current_room = roomsList[num]
+
+    return current_room, player.rect.x, player.rect.y, num
+
+def goRight(num, roomsList, player):
+    # ROOM 1 -> ROOM 3
+    if num == 1: 
+        num = 3
+        player.rect.x, player.rect.y = leftPos(player)
+
+    # ROOM 2 -> DEADEND
+    elif num == 2:
+        pass
+
+    # ROOM 3 -> ROOM 4
+    elif num == 3:
+        num = 4
+        player.rect.x, player.rect.y = topPos(player)
+
+    # ROOM 4 -> DEAD END
+    elif num == 4:
+        pass
+
+    # ROOM 5 -> ROOM 3
+    elif num == 5:
+        num = 3
+        player.rect.x, player.rect.y = botPos(player)
+
+    # ROOM 6 -> ROOM 5
+    elif num == 6:
+        num = 5   
+        player.rect.x, player.rect.y = botPos(player)
+
+    # ROOM 7 -> ROOM 6
+    elif num == 7:
+        num = 6  
+        player.rect.x, player.rect.y = leftPos(player)
+
+    # ROOM 8 -> ROOM 10
+    elif num == 8:
+        num = 10 
+        player.rect.x, player.rect.y = botPos(player)
+
+    # ROOM 9 xxx
+
+    # ROOM 10 -> ROOM 8
+    elif num == 10:
+        num = 8
+        player.rect.x, player.rect.y = rightPos(player)
+
+    # ROOM 11 -> GOAL
+    else:
+        pass
+
+    current_room = roomsList[num]
+
+    return current_room, player.rect.x, player.rect.y, num
+    
+def goDown(num, roomsList, player):
+    # ROOM 0 -> ROOM 1
+    if num == 0:
+        num = 1
+        player.rect.x, player.rect.y = topPos(player)
+
+    # ROOM 1 -> ROOM 4
+    if num == 1: 
+        num = 4
+        player.rect.x, player.rect.y = leftPos(player)
+
+    # ROOM 2 -> ROOM 2
+    if num == 2:
+        num = 2
+        player.rect.x, player.rect.y = botPos(player)
+
+    # ROOM 3 -> ROOM 5
+    if num == 3:
+        num = 5
+        player.rect.x, player.rect.y = rightPos(player)
+
+    # ROOM 4 -> DEAD END
+    if num == 4:
+        pass
+
+    # ROOM 5 -> ROOM 6
+    if num == 5:
+        num = 6
+        player.rect.x, player.rect.y = rightPos(player)
+
+    # ROOM 6 -> ROOM 10
+    if num == 6:
+        num = 10   
+        player.rect.x, player.rect.y = leftPos(player)
+
+    # ROOM 7 xxx
+
+    # ROOM 8 -> ROOM 9
+    if num == 8:
+        num = 9
+        player.rect.x, player.rect.y = topPos(player)
+
+    # ROOM 9 -> DEAD END
+    if num == 9:
+        pass
+
+    # ROOM 10 -> ROOM 10
+    if num == 10:
+        num = 10
+        player.rect.x, player.rect.y = botPos(player)
+
+    # ROOM 11 xxx
+
+
+    current_room = roomsList[num]
+
+    return current_room, player.rect.x, player.rect.y, num
+
+def goUp(num, roomsList, player):
+
+    # ROOM 1 -> ROOM 0
+    if num == 1: 
+        num = 0
+        player.rect.x, player.rect.y = botPos(player)
+
+    # ROOM 2 -> ROOM 1
+    if num == 2:
+        num = 1
+        player.rect.x, player.rect.y = leftPos(player)
+
+    # ROOM 3 xxx
+
+    # ROOM 4 -> ROOM 3
+    if num == 4:
+        num = 3
+        player.rect.x, player.rect.y = rightPos(player)
+
+    # ROOM 5 -> DEAD END
+    if num == 5:
+        pass
+
+    # ROOM 6 xxx
+
+    # ROOM 7 xxx
+
+    # ROOM 8 xxx
+
+    # ROOM 9 -> ROOM 8
+    if num == 9:
+        num = 8
+        player.rect.x, player.rect.y = botPos(player)
+
+    # ROOM 10 -> ROOM 11
+    if num == 10:
+        num = 11
+        player.rect.x, player.rect.y = leftPos(player)
+
+    # ROOM 11 -> ROOM 11
+    if num == 11:
+        num = 11
+        player.rect.x, player.rect.y = topPos(player)
+
+    current_room = roomsList[num]
+
+    return current_room, player.rect.x, player.rect.y, num
 
 
 def main():
@@ -44,10 +272,10 @@ def main():
     movingsprites.add(player)
  
     #This list i(s used for toggling between rooms if the user goes through the doors
-    roomsList = createRoomList()
+    roomsList = createRoomsList()
  
-    current_room_no = 1
-    current_room = roomsList[current_room_no-1]
+    current_room_no = 0
+    current_room = roomsList[current_room_no]
  
     clock = pygame.time.Clock()
  
@@ -93,65 +321,21 @@ def main():
 
         #GO TO THE LEFT
         if player.rect.x < -15:
-            # ROOM 1 -> ROOM 2
-            if current_room_no == 1:
-                current_room_no = 2
-                current_room = roomsList[current_room_no-1]
-                player.rect.x = 400
-                player.rect.y = 0
-
-            # ROOM 2 -> ROOM 2
-            elif current_room_no == 2:
-                current_room_no = 2
-                current_room = roomsList[current_room_no-1]
-                player.rect.x = 0
-                player.rect.y = 300
-            
-            # ROOM 3 -> ROOM 3
-            elif current_room_no == 3:
-                current_room_no = 1
-                current_room = roomsList[current_room_no-1]
-                player.rect.x = 790
-
-            # ROOM 4 -> ROOM 1
-            elif current_room_no == 4:
-                current_room_no = 1   
-                current_room = roomsList[current_room_no-1]
-                player.rect.x = 400
-                player.rect.y = 600
-
-            # ROOM 5 xx
-            else:
-                current_room_no = 3   
-                current_room = roomsList[current_room_no-1]
- 
+            current_room, player.rect.x, player.rect.y, current_room_no = goLeft(current_room_no, roomsList, player)
+            print(current_room.num)
 
         #GO TO THE RIGHT
         if player.rect.x > 801:
-            # ROOM 1 -> ROOM 3
-            if current_room_no == 1:
-                current_room_no = 3
-                current_room = roomsList[current_room_no-1]
-                player.rect.x = 0
-
-            # ROOM 2 -> DEAD END
-
-            # ROOM 3 -> ROOM 4
-            elif current_room_no == 3:
-                current_room_no = 4
-                current_room = roomsList[current_room_no-1]
-                player.rect.x = 400
-                player.rect.y = 0
-            
-            # ROOM 4 -> DEAD END
-
-            # ROOM 5 -> ROOM 3
-            else:
-                current_room_no = 3
-                current_room = roomsList[current_room_no-1]
-                player.rect.x = 400
-                player.rect.y = 600
- 
+            current_room, player.rect.x, player.rect.y, current_room_no = goRight(current_room_no, roomsList, player)
+            print(current_room.num)
+        #GO DOWN
+        if player.rect.y > 600:
+            current_room, player.rect.x, player.rect.y, current_room_no = goDown(current_room_no, roomsList, player)
+            print(current_room.num)
+        #GO UP
+        if player.rect.y < 15:
+            current_room, player.rect.x, player.rect.y, current_room_no = goUp(current_room_no, roomsList, player)
+            print(current_room.num)
         # --- Drawing ---
 
         #Black background color
--- wallsRooms.py
+++ wallsRooms.py
@@ -37,13 +37,40 @@ class Room(object):
     def __init__(self):
         self.wallsList = pygame.sprite.Group()
         self.enemy_sprites = pygame.sprite.Group()
+
  
+# START ROOM
+class RoomStart(Room):
+    """This creates all the walls in room"""  
+    def __init__(self, num):
+        super().__init__()
+        self.num = num
+        # Make the walls. (x-axis, y-axis, width, height)
+ 
+        # This is a list of walls. Each is in the form [x, y, width, height]
+        walls = [[20, 0, 800, 20, WHITE], #top
+
+                 [0, 0, 20, 600, WHITE], #left
+
+                 [780, 0, 20, 600, WHITE], #right
+
+                 [20, 580, 320, 20, WHITE], #bottom
+                 [450, 580, 330, 20, WHITE], 
+                
+                 [190, 50, 20, 500, BLUE]
+                ]
+ 
+        # Loop through the list. Create the wall, add it to the list
+        for item in walls: #[x, y, width, height]
+            wall = Wall(item[0], item[1], item[2], item[3], item[4])
+            self.wallsList.add(wall)
 
 # 4 WALLS ROOM
 class RoomFull(Room):
     """This creates all the walls in room"""  
-    def __init__(self):
+    def __init__(self, num):
         super().__init__()
+        self.num = num
         # Make the walls. (x-axis, y-axis, width, height)
  
         # This is a list of walls. Each is in the form [x, y, width, height]
@@ -70,9 +97,9 @@ class RoomFull(Room):
 # FULL TOP ROOM
 class RoomFullTop(Room):
     """This creates all the walls in room 3"""
-    def __init__(self):
+    def __init__(self, num):
         super().__init__()
-         
+        self.num = num
          #[x, y, width, height, color]
         walls = [[20, 0, 600, 20, WHITE], #top
 
@@ -98,8 +125,9 @@ class RoomFullTop(Room):
 # FULL LEFT ROOM
 class RoomFullLeft(Room): 
     """This creates all the walls in room 4"""
-    def __init__(self):
+    def __init__(self, num):
         super().__init__()
+        self.num = num
         # Make the walls. (x-axis, y-axis, width, height)
  
         # This is a list of walls. Each is in the form [x, y, width, height]
@@ -129,8 +157,9 @@ class RoomFullLeft(Room):
 # FULL RIGHT ROOM
 class RoomFullRight(Room):
     """This creates all the walls in room"""  
-    def __init__(self):
+    def __init__(self, num):
         super().__init__()
+        self.num = num
         # Make the walls. (x-axis, y-axis, width, height)
  
         # This is a list of walls. Each is in the form [x, y, width, height]
@@ -156,8 +185,9 @@ class RoomFullRight(Room):
 # FULL BOTTOM ROOM
 class RoomFullBottom(Room):
     """This creates all the walls in room"""  
-    def __init__(self):
+    def __init__(self, num):
         super().__init__()
+        self.num = num
         # Make the walls. (x-axis, y-axis, width, height)
  
         # This is a list of walls. Each is in the form [x, y, width, height]
@@ -185,3 +215,61 @@ class RoomFullBottom(Room):
 # WE CAN CREATE A DIFFERENT DECORATION CLASS / METHOD HERE AND ADD TO THE ROOM
 # DONT NEED TO BE TOO FANCY
 # ROOMS CAN HAVE THE SAME DECORATION
+
+# FULL LEFT RIGHT ROOM
+class RoomFullLeftRight(Room): 
+    """This creates all the walls in room 4"""
+    def __init__(self, num):
+        super().__init__()
+        self.num = num
+        # Make the walls. (x-axis, y-axis, width, height)
+ 
+        # This is a list of walls. Each is in the form [x, y, width, height]
+        walls = [[20, 0, 320, 20, WHITE], #top
+                 [450, 0, 330, 20, WHITE],
+
+                 [0, 0, 20, 600, WHITE], #left
+
+                 [780, 0, 20, 600, WHITE], #right
+
+                 [20, 580, 320, 20, WHITE], #bottom
+                 [450, 580, 330, 20, WHITE], 
+
+                 [190, 50, 20, 500, BLUE],
+                 [290, 50, 20, 500, BLUE],
+                 [390, 50, 20, 500, BLUE],
+                 [490, 50, 20, 500, BLUE],
+                 [590, 50, 20, 500, BLUE]
+                ]
+ 
+        # Loop through the list. Create the wall, add it to the list
+        for item in walls: #[x, y, width, height]
+            wall = Wall(item[0], item[1], item[2], item[3], item[4])
+            self.wallsList.add(wall)
+ 
+# FULL TOP BOTTOM ROOM
+class RoomFullTopBottom(Room):
+    """This creates all the walls in room 3"""
+    def __init__(self, num):
+        super().__init__()
+        self.num = num
+         #[x, y, width, height, color]
+        walls = [[20, 0, 600, 20, WHITE], #top
+
+                 [0, 0, 20, 250, WHITE], #left
+                 [0, 350, 20, 250, WHITE],
+
+                 [780, 0, 20, 250, WHITE], #right
+                 [780, 350, 20, 250, WHITE],
+
+                 [20, 580, 600, 20, WHITE], #bottom
+
+                 [190, 50, 20, 500, BLUE],
+                 [290, 50, 20, 500, PURPLE],
+
+                 [20, 0, 760, 20, WHITE], #Any walls underneath the white walls are extras
+                ]
+ 
+        for item in walls:
+            wall = Wall(item[0], item[1], item[2], item[3], item[4])
+            self.wallsList.add(wall)
